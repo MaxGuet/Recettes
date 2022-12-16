964cf44116885e4e9ffcdfbac1ee8e7297938950
MaxGuet <maxence.guet@gmail.com>
2022-12-16 11:42:07 +0100

    JS function working / style trials

--- index.js
+++ index.js
@@ -1,78 +1,84 @@
-// const rec = document.querySelector("#receipe");
+const rec = document.querySelector("#receipe");
 const header = document.querySelector("header");
+const ticket = document.querySelector(".ticket");
+const ticket2 = document.querySelector(".ticket2");
+const ticket3 = document.querySelector(".ticket3");
+const ticket4 = document.querySelector(".ticket4");
 
-// let pates = document.querySelector("#pates");
-// let poireau = document.querySelector("#poireau");
-// let riz = document.querySelector("#riz");
-// let patates = document.querySelector("#patate");
-// let polenta = document.querySelector("#polenta");
-// let carotte = document.querySelector("#carotte");
-// let fondVeau = document.querySelector("#fondDeVeau");
-// let oignon = document.querySelector("#oignon");
-// let beurre = document.querySelector("#beurre");
-// let lait = document.querySelector("#lait");
-// let haricotR = document.querySelector("#haricotRouge");
-// let boeuf = document.querySelector("#boeuf");
-// let tomate = document.querySelector("#tomate");
-// let huileOlive = document.querySelector("#huileOlive");
-// let poivron = document.querySelector("#poivron");
-// let ail = document.querySelector("#ail");
-// let asperge = document.querySelector("#asperge");
-// let parmesan = document.querySelector("#parmesan");
-// let vinBlanc = document.querySelector("#vinBlanc");
-// let cumin = document.querySelector("#cumin");
-// let muscade = document.querySelector("#muscade");
-// let conté = document.querySelector("#conté");
-// let persil = document.querySelector("#persil");
+let pates = document.querySelector("#pates");
+let poireau = document.querySelector("#poireau");
+let riz = document.querySelector("#riz");
+let patates = document.querySelector("#patate");
+let polenta = document.querySelector("#polenta");
+let carotte = document.querySelector("#carotte");
+let fondVeau = document.querySelector("#fondDeVeau");
+let oignon = document.querySelector("#oignon");
+let beurre = document.querySelector("#beurre");
+let lait = document.querySelector("#lait");
+let haricotR = document.querySelector("#haricotRouge");
+let boeuf = document.querySelector("#boeuf");
+let tomate = document.querySelector("#tomate");
+let huileOlive = document.querySelector("#huileOlive");
+let poivron = document.querySelector("#poivron");
+let ail = document.querySelector("#ail");
+let asperge = document.querySelector("#asperge");
+let parmesan = document.querySelector("#parmesan");
+let vinBlanc = document.querySelector("#vinBlanc");
+let cumin = document.querySelector("#cumin");
+let muscade = document.querySelector("#muscade");
+let comté = document.querySelector("#comté");
+let persil = document.querySelector("#persil");
 
-// const cook = () => {
-//   if (
-//     riz.checked === true &&
-//     oignon.checked === true &&
-//     fondVeau.checked === true &&
-//     beurre.checked === true &&
-//     asperge.checked === true &&
-//     parmesan.checked === true &&
-//     vinBlanc.checked === true
-//   ) {
-//     console.log("Risotto aux asperges!");
-//   }
-//   if (
-//     patates.checked === true &&
-//     beurre.checked === true &&
-//     lait.checked === true &&
-//     muscade.checked === true
-//   ) {
-//     console.log("Purée!");
-//   }
-//   if (
-//     riz.checked === true &&
-//     haricotR.checked === true &&
-//     tomate.checked === true &&
-//     huileOlive.checked === true &&
-//     boeuf.checked === true &&
-//     carotte.checked === true &&
-//     poivron.checked === true &&
-//     oignon.checked === true &&
-//     ail.checked === true &&
-//     huileOlive.checked === true
-//   ) {
-//     console.log("Chili Con Carne!");
-//   }
-//   if (
-//     patates.checked === true &&
-//     beurre.checked === true &&
-//     conté.checked === true &&
-//     boeuf.checked === true &&
-//     oignon.checked === true &&
-//     ail.checked === true &&
-//     persil.checked === true
-//   ) {
-//     console.log("Hachis Parmentier!");
-//   }
-// };
-
-// cook();
+rec.addEventListener("click", () => {
+  if (
+    riz.checked === true &&
+    oignon.checked === true &&
+    fondVeau.checked === true &&
+    beurre.checked === true &&
+    asperge.checked === true &&
+    parmesan.checked === true &&
+    vinBlanc.checked === true
+  ) {
+    ticket2.style.visibility = "visible";
+    ticket2.style.opacity = "1";
+  }
+  if (
+    patates.checked === true &&
+    beurre.checked === true &&
+    lait.checked === true &&
+    muscade.checked === true
+  ) {
+    ticket.style.visibility = "visible";
+    ticket.style.opacity = "1";
+  }
+  if (
+    riz.checked === true &&
+    haricotR.checked === true &&
+    tomate.checked === true &&
+    huileOlive.checked === true &&
+    boeuf.checked === true &&
+    carotte.checked === true &&
+    poivron.checked === true &&
+    oignon.checked === true &&
+    ail.checked === true &&
+    huileOlive.checked === true
+  ) {
+    ticket3.style.visibility = "visible";
+    ticket3.style.opacity = "1";
+  }
+  if (
+    patates.checked === true &&
+    beurre.checked === true &&
+    comté.checked === true &&
+    boeuf.checked === true &&
+    oignon.checked === true &&
+    ail.checked === true &&
+    persil.checked === true
+  ) {
+    ticket3.style.visibility = "visible";
+    ticket3.style.opacity = "1";
+  }
+});
 
 let lastScroll = 0;
 
